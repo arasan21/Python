9b877fc3ec2f89a36f20703b47d95d2a6ea70e83
Poovarasan P <arasan_21@hotmail.com>
2018-12-10 23:04:28 +0530

    Add files via upload

---
+++ file_mail_count/file_mail_count.py
@@ -0,0 +1,38 @@
+# program to read data from file and fetch email from it and its count and store it in sqlite database
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    email = pieces[1]
+    organizationDomainlist = email.split('@')
+    organizationDomain = organizationDomainlist[1]
+    print(organizationDomain)
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (organizationDomain,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (organizationDomain,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (organizationDomain,))
+    conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
